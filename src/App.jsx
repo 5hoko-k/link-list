2a47411e4d1ac90ca614d8459d0016556132e0f8
Shoko Kimera <shokokimera@gmail.com>
2024-01-06 22:15:37 +0300

    change the description and add a quote

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,14 @@ function App() {
           <img src={anithril} className="logo circle-svg" alt="Anithril logo" />
         </header>
         <p>
-        Dear amazing supporters, your support keeps our shared content alive. Inspired by anime, I create gripping scene edits that once gave me chills, now shared worldwide through Anithril. Plus, as a self-taught web developer, I thrive on crafting impactful sites like this one. I'm in a tight spot and reaching out for help—just around <b>$20</b> would mean the world to me. 
+          Hey there, incredible supporters! Your encouragement fuels our vibrant community. Drawing inspiration from anime, I craft mesmerizing scene edits that used to give me chills—now they're thrilling audiences worldwide via Anithril. Additionally, as a self-taught web developer, I specialize in creating impactful sites, like the one you're experiencing.</p>
+
+        <p>  I'm at a pivotal point and extending a hand for support would make an immense difference. Your backing helps sustain this creative journey, allowing us to elevate our content and expand our reach together. Thank you for being part of this incredible adventure!
         </p>
+        <blockquote className='quote'>
+          When you have to save someone, they're usually in a scary situation. And they're usually really alone. So you make them think, 'I am here.'
+          <footer><cite>-- All MIght, My Hero Academia</cite></footer>
+        </blockquote>
         <a href="https://www.buymeacoffee.com/anithril"><img src="https://img.buymeacoffee.com/button-api/?text=Help me out&emoji=🤝&slug=anithril&button_colour=c9184a&font_colour=ffffff&font_family=Inter&outline_colour=ffffff&coffee_colour=FFDD00" /></a>
       </article>
       <p className="read-the-docs">
